60d9a523332dcedd2338aae5b7c85619a2cf0093
vouks32 <85527577+vouks32@users.noreply.github.com>
2025-07-01 12:36:35 +0100

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+
+// Configuration Firebase (à remplacer par vos propres infos)
+const firebaseConfig = {
+  apiKey: "AIzaSyBDFygmSGuKr7T4Eg4zVzmMzLbXq9bMsek",
+  authDomain: "community-8d9a2.firebaseapp.com",
+  projectId: "community-8d9a2",
+  storageBucket: "community-8d9a2.firebasestorage.app",
+  messagingSenderId: "355282374642",
+  appId: "1:355282374642:web:4997f3273d1054c336256b",
+  measurementId: "G-VF5WXEFQJX"
+};
+
+export { firebaseConfig };
